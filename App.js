ca577350dd37bbedc22a6f3ec28bb5fd2488687c
DrNKGupta <nidhish.cu@gmail.com>
2024-07-26 17:59:37 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View,} from 'react-native';
+
+const App=()=>{
+
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;

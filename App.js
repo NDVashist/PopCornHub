f4fa380b0cd1da6e9dad6ea1af67db9a778a8337
DrNKGupta <nidhish.cu@gmail.com>
2024-07-26 19:45:22 +0530

    update

--- App.js
+++ App.js
@@ -1,46 +1,24 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from './screens/SplashScreen';
 import HomeScreen from './screens/HomeScreen';
 import SearchScreen from './screens/SearchScreen';
 import DetailsScreen from './screens/DetailsScreen';
-import SplashScreen from './screens/SplashScreen';
 
 const Stack = createStackNavigator();
-const Tab = createBottomTabNavigator();
 
-function MainTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Search" component={SearchScreen} />
-    </Tab.Navigator>
-  );
-}
-
-function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Splash">
-        <Stack.Screen
-          name="Splash"
-          component={SplashScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="MainTabs"
-          component={MainTabs}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: 'Details' }}
-        />
+        <Stack.Screen name="Splash" component={SplashScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
 export default App;
--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,49 +1,16 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Image, StyleSheet, Animated, Easing } from 'react-native';
-import { BlurView } from '@react-native-community/blur';
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 
 const SplashScreen = ({ navigation }) => {
-  const bounceValue = useRef(new Animated.Value(1)).current;
-  const blurValue = useRef(new Animated.Value(10)).current;
-
   useEffect(() => {
-    Animated.parallel([
-      Animated.sequence([
-        Animated.timing(bounceValue, {
-          toValue: 1.2,
-          duration: 500,
-          easing: Easing.bounce,
-          useNativeDriver: true,
-        }),
-        Animated.timing(bounceValue, {
-          toValue: 1,
-          duration: 500,
-          easing: Easing.bounce,
-          useNativeDriver: true,
-        }),
-      ]),
-      Animated.timing(blurValue, {
-        toValue: 0,
-        duration: 3000,
-        useNativeDriver: false,
-      }),
-    ]).start();
-
     setTimeout(() => {
-      navigation.replace('Home');
+      navigation.replace('Home'); // Navigate to Home screen
     }, 3000); // Display splash screen for 3 seconds
-  }, [navigation, bounceValue, blurValue]);
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
-      <BlurView
-        style={styles.absolute}
-        blurType="light"
-        blurAmount={blurValue}
-      />
-      <Animated.View style={[styles.imageContainer, { transform: [{ scale: bounceValue }] }]}>
-        <Image source={require('../assets/splash.png')} style={styles.image} />
-      </Animated.View>
+      <Image source={require('../assets/splash.png')} style={styles.image} />
     </View>
   );
 };
@@ -55,17 +22,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'black',
   },
-  imageContainer: {
+  image: {
     width: '80%',
     height: '25%',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
     resizeMode: 'contain',
-  },
-  absolute: {
-    ...StyleSheet.absoluteFillObject,
   },
 });
 
